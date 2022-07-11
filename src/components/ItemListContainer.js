5cfc16a956d1227fd83ff570e784d620d7db2adb
Franco <francoazari@gmail.com>
2022-07-11 20:34:44 -0300

    Correciones Desafío: Catálogo con MAPS y Promises

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,14 +1,22 @@
+import { useEffect, useState } from "react";
 import ItemList from "./itemList";
 
 
 function ItemListContainer(props) {
 
+    const [products, setProducts] = useState([]);
 
+    useEffect(() => {
+
+        fetch('https://fakestoreapi.com/products?limit=5')
+            .then(res=>res.json())
+            .then(json=>setProducts(json));
+    }, []);
 
     return(
         <>
             <h1>{props.greeting}</h1>
-            <ItemList />
+            <ItemList products={products}/>
         </>
     );
 }
--- src/components/itemList.js
+++ src/components/itemList.js
@@ -1,19 +1,7 @@
-import { useEffect, useState } from "react";
 import Item from "./item";
 
 
-function ItemList(props){
-
-    const [products, setProducts] = useState([]);
-
-    useEffect(() => {
-
-        fetch('https://fakestoreapi.com/products?limit=5')
-            .then(res=>res.json())
-            .then(json=>setProducts(json));
-    }, []);
-
-
+function ItemList({products}){
 
     return(
         <div style={styles.listItems}>
