5dda01ecd6986d7d84f894d246aa3c80e5ab5fc7
Franco <francoazari@gmail.com>
2022-07-11 23:14:29 -0300

    agregando useParams

--- src/components/ItemDetailContainer.js
+++ src/components/ItemDetailContainer.js
@@ -1,5 +1,6 @@
 import { CircularProgress } from "@mui/material";
 import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import ItemDetail from "./ItemDetail";
 
 
@@ -8,24 +9,14 @@ function ItemDetailContainer(props) {
 
     const [product, setProduct] = useState([]);
     const [loaded, setLoaded] = useState(true);
-    const querystring = window.location.search;
-    const params = new URLSearchParams(querystring);
-    const [productId, setProductId] = useState();
+    const { productId } = useParams();
     
     useEffect(() => {
-        setProductId(params.get('productId'));    
-    }, []);
-
-    
-    
-    useEffect(() => {
-        if(productId){
             fetch(`https://fakestoreapi.com/products/${productId}`)
             .then(res=>res.json())
             .then(data=>setProduct(data))
             .catch(err=>console.log(err))
             .finally(()=>setLoaded(false))
-        }
         
     }, [productId]);
 
