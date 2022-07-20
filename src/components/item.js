c8dc11396df5c3ea577134e3c8b947dff7e154fd
Franco <francoazari@gmail.com>
2022-07-20 15:13:15 -0300

    estilos en el componente item

--- src/components/item.js
+++ src/components/item.js
@@ -3,16 +3,18 @@ import { Link } from "react-router-dom";
 function Item(props) {
     return (
         <div style={styles.article}>
-            <img
-                style={styles.articleImage}
-                src={props.productInfo.image}
-                alt={props.productInfo.title}
-            />
-            <Link to={`/item/${props.productInfo.id}`}>
-                <p style={styles.title}>{props.productInfo.title}</p>
-            </Link>
+            <div style={styles.image}>
+                <img
+                    style={styles.articleImage}
+                    src={props.productInfo.image}
+                    alt={props.productInfo.title}
+                />
+            </div>
+            <p style={styles.title}>{props.productInfo.title}</p>
             <div style={styles.price}>${props.productInfo.price}</div>
-            <p style={styles.description}>{props.productInfo.description}</p>
+            <Link to={`/item/${props.productInfo.id}`}>
+                <button style={styles.verDetalle}>Ver detalle</button>
+            </Link>
         </div>
     );
 }
@@ -21,17 +23,27 @@ const styles = {
     article: {
         display: "flex",
         flexDirection: "column",
+        justifyContent: "space-between",
         width: "130px",
-        height: "200px",
+        height: "180px",
         margin: "5px",
         padding: "10px",
         overflow: "hidden",
         borderRadius: "5px",
         backgroundColor: "#ebebeb"
     },
+    image: {
+        width: "100%",
+        height: "50%",
+        backgroundColor: "white",
+        borderRadius: "5px",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center"
+    },
     articleImage: {
-        maxWidth: "100%",
-        maxHeight: "40%",
+        maxWidth: "90%",
+        maxHeight: "90%",
         objectFit: "cover"
     },
     title: {
@@ -47,14 +59,15 @@ const styles = {
         fontSize: "0.8em",
         textAlign: "left"
     },
-    description: {
-        height: "80px",
-        fontSize: "0.8em",
-        margin: "5px 0",
-        textAlign: "left",
-        overflowY: "auto",
-        overflowX: "hidden",
-        textOverflow: "ellipsis"
+    verDetalle: {
+        width: "100%",
+        border: "0",
+        backgroundColor: "#0b1c42",
+        borderRadius: "5px",
+        padding: "5px",
+        color: "#FFF",
+        marginTop: "10px",
+        cursor: "pointer"
     }
 };
 
