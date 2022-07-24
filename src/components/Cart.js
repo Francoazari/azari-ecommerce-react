23f71b1861ad3636eafcf3bfde0dccbdeeb24f28
Franco <francoazari@gmail.com>
2022-07-24 18:54:08 -0300

    Desafío: Item Collection II

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,20 +1,64 @@
 import { useContext } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { contexto } from "../context/CartContext";
 import ItemCart from "./ItemCart";
+import { db } from "../firabase/firebase";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import swal from "sweetalert";
 
 const Cart = () => {
-    const { products, removeItem, getTotal } = useContext(contexto);
+    const { products, removeItem, getTotal, clear } = useContext(contexto);
+    let navigate = useNavigate();
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const ventasCollection = collection(db, "ventas");
+        addDoc(ventasCollection, {
+            buyer: {
+                name: `${event.target["name"].value} ${event.target["lastname"].value}`,
+                phone: event.target["phone"].value,
+                email: event.target["email"].value
+            },
+            items: products,
+            date: serverTimestamp(),
+            total: getTotal()
+        })
+            .then((result) => {
+                clear();
+                swal("Compra realizada con éxito", "ID Venta: " + result.id, "success").then((value) => {
+                    navigate("/");
+                });
+            })
+            .catch((err) => {
+                swal("¡Lo sentimos!", "Ha ocurrido un error con su compra. Por favor intente nuevamente.", "error");
+                console.error(err);
+            });
+    };
 
     return (
         <>
             <h2>Cart</h2>
             {products.length > 0 && (
-                <div style={styles.cartContainer}>
-                    {products.map((product) => {
-                        return <ItemCart key={product.id} item={product} removeItem={removeItem} />;
-                    })}
-                    <div style={styles.total}>Total: ${getTotal().toFixed(2)}</div>
+                <div style={styles.container}>
+                    <div style={styles.cartContainer}>
+                        {products.map((product) => {
+                            return <ItemCart key={product.id} item={product} removeItem={removeItem} />;
+                        })}
+                        <div style={styles.total}>Total: ${getTotal().toFixed(2)}</div>
+                    </div>
+                    <div style={styles.datosUsuario}>
+                        <h3>Finalizar compra</h3>
+                        <form onSubmit={(e) => handleSubmit(e)}>
+                            <input style={styles.inputForm} type="text" name="name" placeholder="Nombre" required />
+                            <input style={styles.inputForm} type="text" name="lastname" placeholder="Apellido" required />
+                            <input style={styles.inputForm} type="email" name="email" placeholder="Email" required />
+                            <input style={styles.inputForm} type="phone" name="phone" placeholder="Telefono" required />
+                            <button style={styles.buttonForm} type="submit">
+                                Confirmar compra
+                            </button>
+                        </form>
+                    </div>
                 </div>
             )}
 
@@ -31,6 +75,11 @@ const Cart = () => {
 };
 
 const styles = {
+    container: {
+        display: "flex",
+        flexDirection: "row",
+        gap: "10px"
+    },
     noProducts: {
         width: "100%",
         textAlign: "center",
@@ -48,9 +97,10 @@ const styles = {
     },
     cartContainer: {
         width: "80%",
+        minWidth: "700px",
         display: "flex",
         flexDirection: "column",
-        justifyContent: "center",
+        justifyContent: "flex-start",
         alignItems: "center"
     },
     irTienda: {
@@ -64,6 +114,24 @@ const styles = {
         marginTop: "10px",
         cursor: "pointer",
         textDecoration: "none"
+    },
+    inputForm: {
+        width: "100%",
+        height: "30px",
+        borderRadius: "5px",
+        border: "1px solid #C7C7C7",
+        padding: "5px",
+        marginBottom: "10px"
+    },
+    buttonForm: {
+        width: "100%",
+        height: "30px",
+        borderRadius: "5px",
+        border: "0",
+        padding: "5px",
+        marginBottom: "10px",
+        backgroundColor: "#027900",
+        color: "#fff"
     }
 };
 
